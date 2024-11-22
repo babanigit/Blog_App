4dec8444ab1358d2ce4ade400d87b009ff4fc0e5
aniket panchal <aniketpanchal07@gmail.com>
2024-11-23 00:06:33 +0530

    done with the input field of the singup button

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -12,18 +12,32 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Sign Up.',
-            style: TextStyle(
-              fontSize: 50,
-              fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          AuthField(hintText: 'Email')
-        ],
+            SizedBox(
+              height: 30,
+            ),
+            AuthField(hintText: 'Name'),
+            SizedBox(
+              height: 15,
+            ),
+            AuthField(hintText: 'Email'),
+            SizedBox(
+              height: 15,
+            ),
+            AuthField(hintText: 'Password')
+          ],
+        ),
       ),
     );
   }
