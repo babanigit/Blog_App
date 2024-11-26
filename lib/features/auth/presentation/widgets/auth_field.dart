079347ce97afa727cac6b798a0e38bedfdae4aa6
aniket panchal <aniketpanchal07@gmail.com>
2024-11-26 14:04:57 +0530

    done withe the signup page

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -2,12 +2,28 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
-  const AuthField({Key? key, required this.hintText}) : super(key: key);
+  final TextEditingController controller;
+  final bool isObscureText;
+
+  const AuthField(
+      {Key? key,
+      required this.hintText,
+      required this.controller,
+      this.isObscureText = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(hintText: hintText),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "$hintText is missing!";
+        }
+        return null;
+      },
+      obscureText:isObscureText,
     );
   }
 }
